0571d754c3549c32036c9505632b7e9eef400d1f
Lucas Ywamoto <92839243+lucasywamoto@users.noreply.github.com>
2023-10-27 11:50:32 -0400

    Initializing a Vite Project

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import Navbar from './components/Navbar'
+import './App.css'
+import Main from './components/Main'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Main />
+    </>
+  )
+}
+
+export default App
